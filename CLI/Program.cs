9d9d62458cc4010cedd878e43b0992bf82ce1c93
lucianacasasrobles-2728 <luciana_casas_robles@hotmail.com>
2026-09-18 08:47:48 +0200

    Add CLI project files

---
+++ CLI/Program.cs
@@ -0,0 +1,25 @@
+﻿using CLI.UI;
+using InMemoryRepositories;
+using RepositoryContracts;
+
+namespace CLI;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Starting CLI Application...");
+
+        IUserRepository userRepository = new UserInMemoryRepository();
+        ICommentRepository commentRepository = new CommentInMemoryRepository();
+        IPostRepository postRepository = new PostInMemoryRepository();
+
+        CliApp app = new CliApp(
+            userRepository,
+            commentRepository,
+            postRepository
+        );
+
+        await app.StartAsync();
+    }
+}
